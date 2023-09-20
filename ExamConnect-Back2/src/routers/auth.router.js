e190dab6926da0365562880c6677d860686155dc
mohamed haddad <m.haddad@sysbas.com>
2023-09-20 18:57:49 +0100

    i implemented the Authentication API including the Login using JWT for autorizations but stil working on it !

---
+++ ExamConnect-Back2/src/routers/auth.router.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const connection = require("../DataBaseConf/MySqlConnection");
+const jwt = require("jsonwebtoken");
+
+router.get("/login", (req, res) => {
+     console.log("login");
+     console.log(process.env.JWT_SECRET);
+     res.status(200).json({ message: process.env.SERVER_PORT });
+});
+
+
+router.post("/login", (req, res) => {
+
+     const email = req.body.email;
+     const password = req.body.password;
+     const selectQuery = "SELECT * FROM user WHERE email = ? AND password = ?";
+     connection.query(selectQuery, [email, password], (err, rows) => {
+          if (err) {
+               console.log(err);
+               res.status(500).json({ message: "Internal server error" });
+          } else {
+               if (rows.length > 0) {
+                    const user = rows[0];
+                    const accessToken = jwt.sign(
+                         { id: user.id },
+                         process.env.JWT_SECRET_KEY,
+                         {
+                              expiresIn: "24h",
+                         }
+                    );
+                    res.status(200).json({
+                         message: "login success",
+                         user: {
+                              fullName: user.fullName,
+                              isAdmin: user.isAdmin,
+                         },
+                         accessToken: accessToken,
+                    });
+               } else {
+                    res.status(401).json({ message: "Invalid Credentials !" });
+               }
+          }
+     });
+     
+});
+
+module.exports = router;
