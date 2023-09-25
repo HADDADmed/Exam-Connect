b4211f056c210f9c0ea9d92d9ce0802053cc0e52
HADDAD <mohh2045@gmail.com>
2023-09-25 23:10:18 +0100

    Improved route guard for user authentication and admin access control

--- ExamConnect-Front/src/router/index.js
+++ ExamConnect-Front/src/router/index.js
@@ -2,154 +2,168 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 import NProgress from "nprogress/nprogress.js";
 
+function guardMyRoutes(to, from, next) {
+     const user = JSON.parse(localStorage.getItem("user"));
+     const accessToken = localStorage.getItem("accessToken");
+
+     // Check if the user is logged in
+     if (user && accessToken) {
+         next();
+     } else if (to.name === "examconnect-authentication") {
+         // Allow access to the 'examconnect-authentication' route but clear local storage
+         localStorage.clear();
+         next();
+     } else {
+         // Redirect to the 'examconnect-authentication' route if not logged in
+         next({ name: "examconnect-authentication" });
+     }
+ }
 
 
-
-function lazyLoadVue(view,folder=null) {
-  if(folder) return () => import(`@/views/examconnect/${folder}/${view}.vue`);
-  else return () => import(`@/views/examconnect/${view}.vue`);
+function lazyLoadVue(view, folder = null) {
+     if (folder)
+          return () => import(`@/views/examconnect/${folder}/${view}.vue`);
+     else return () => import(`@/views/examconnect/${view}.vue`);
 }
 function lazyLoadLayout(params) {
-  return () => import(`@/layouts/variations/${params}.vue`);
+     return () => import(`@/layouts/variations/${params}.vue`);
 }
-
-
-
 
 // Set all routes
 const routes = [
-  /*
+     /*
   |
   |--------------------------------------------------------------------------
   | Landing Routes
   |--------------------------------------------------------------------------
   |
   */
-  {
-    path: "/",
-    name: "out",
-    component: lazyLoadVue('AuthenticationPage','Auth'),
-  },{
-    path: "/examconnect",
-    redirect: "/examconnect/examconnect-dashboard",
-    component: lazyLoadLayout('Backend'),
-    children: [
-      {
-        path: "/",
-        name: "landing",
-        component: lazyLoadVue('DashboardView'),
-    
-        },
-      {
-        path: "examconnect-authentication",
-        name: "examconnect-authentication",
-        component: lazyLoadVue('AuthenticationPage','Auth'),
-      },
-      {
-        path: "examconnect-dashboard",
-        name: "examconnect-dashboard",
-        component: lazyLoadVue('DashboardView'),
-      },{
-        path: "exams/examconnect-exams-list",
-        name: "examconnect-exams-list",
-        component: lazyLoadVue('ExamsList','Exams'),
-      },{
-        path: "exams/examconnect-exams-create",
-        name: "examconnect-exams-create",
-        component: lazyLoadVue('CreateExam','Exams'),
-      },{
-        path: "exams/examconnect-exams-exam",
-        name: "examconnect-exams-exam",
-        component: lazyLoadVue('OneExamPageAdmin','Exams'),
-      },{
-        path: "questions/examconnect-questions-list",
-        name: "examconnect-questions-list",
-        component: lazyLoadVue('QuestionsList','Questions'),
-      },{
-        path: "questions/examconnect-questions-create",
-        name: "examconnect-questions-create",
-        component: lazyLoadVue('CreateQuestion','Questions'),
-      },{
-        path: "Users/examconnect-users-list",
-        name: "examconnect-users-list",
-        component: lazyLoadVue('UsersList','Users'),
-      },{
-        path: "Users/examconnect-users-create",
-        name: "examconnect-users-create",
-        component: lazyLoadVue('CreateUser','Users'),
-      },{
-        path: "Users/examconnect-exam-results",
-        name: "examconnect-exam-results",
-        component: lazyLoadVue('ExamResult','Exams'),
-      },
-    ]
+     {
+          path: "/signin",
+          name: "examconnect-authentication",
+          component: lazyLoadVue("AuthenticationPage", "Auth"),
+     },
+     {
+          path: "/examconnect",
+          redirect: "/examconnect/examconnect-dashboard",
+          component: lazyLoadLayout("Backend"),
+          children: [
+               {
+                    path: "/",
+                    name: "landing",
+                    component: lazyLoadVue("DashboardView"),
+               },
+               {
+                    path: "examconnect-dashboard",
+                    name: "examconnect-dashboard",
+                    component: lazyLoadVue("DashboardView"),
+               },
+               {
+                    path: "exams/examconnect-exams-list",
+                    name: "examconnect-exams-list",
+                    component: lazyLoadVue("ExamsList", "Exams"),
+               },
+               {
+                    path: "exams/examconnect-exams-create",
+                    name: "examconnect-exams-create",
+                    component: lazyLoadVue("CreateExam", "Exams"),
+               },
+               {
+                    path: "exams/examconnect-exams-exam",
+                    name: "examconnect-exams-exam",
+                    component: lazyLoadVue("OneExamPageAdmin", "Exams"),
+               },
+               {
+                    path: "questions/examconnect-questions-list",
+                    name: "examconnect-questions-list",
+                    component: lazyLoadVue("QuestionsList", "Questions"),
+               },
+               {
+                    path: "questions/examconnect-questions-create",
+                    name: "examconnect-questions-create",
+                    component: lazyLoadVue("CreateQuestion", "Questions"),
+               },
+               {
+                    path: "Users/examconnect-users-list",
+                    name: "examconnect-users-list",
+                    component: lazyLoadVue("UsersList", "Users"),
+               },
+               {
+                    path: "Users/examconnect-users-create",
+                    name: "examconnect-users-create",
+                    component: lazyLoadVue("CreateUser", "Users"),
+               },
+               {
+                    path: "Users/examconnect-exam-results",
+                    name: "examconnect-exam-results",
+                    component: lazyLoadVue("ExamResult", "Exams"),
+               },
+          ],
+     },
 
-  },
-
-  /*
+     /*
   |
   |--------------------------------------------------------------------------
   | Backend Boxed Routes
   |--------------------------------------------------------------------------
   |
   */
-  {
-    path: "/examconnect-user",
-    component: lazyLoadLayout('Backend'),
-    children: [
-      {
-        path: "examconnect-user-exam",
-        name: "examconnect-user-exam",
-        component: lazyLoadVue('OneExamPageUser','Exams'),
-      },
-      {
-        path: "404",
-        name: "user-error-404",
-        component: lazyLoadVue('404Vue','Erors'),
-      },
-      {
-        path:"401",
-        name:"user-error-401",
-        component: lazyLoadVue('401Vue','Erors'),
-      },
-      {
-        path:"user-error-examSubmited",
-        name:"examSubmited",
-        component: lazyLoadVue('examSubmited','Erors'),
-      }
-      
-    ],
-  },
-
-  
-   
+     {
+          path: "/examconnect-user",
+          component: lazyLoadLayout("Backend"),
+          children: [
+               {
+                    path: "examconnect-user-exam",
+                    name: "examconnect-user-exam",
+                    component: lazyLoadVue("OneExamPageUser", "Exams"),
+               },
+               {
+                    path: "404",
+                    name: "user-error-404",
+                    component: lazyLoadVue("404Vue", "Erors"),
+               },
+               {
+                    path: "401",
+                    name: "user-error-401",
+                    component: lazyLoadVue("401Vue", "Erors"),
+               },
+               {
+                    path: "user-error-examSubmited",
+                    name: "examSubmited",
+                    component: lazyLoadVue("examSubmited", "Erors"),
+               },
+          ],
+     },
 ];
 
 // Create Router
 const router = createRouter({
-  history: createWebHashHistory(),
-  linkActiveClass: "active",
-  linkExactActiveClass: "",
-  scrollBehavior() {
-    return { left: 0, top: 0 };
-  },
-  routes,
+     history: createWebHashHistory(),
+     linkActiveClass: "active",
+     linkExactActiveClass: "",
+     scrollBehavior() {
+          return { left: 0, top: 0 };
+     },
+     routes,
 });
+
+// Router Guards
+router.beforeEach(guardMyRoutes);
 
 // NProgress
 /*eslint-disable no-unused-vars*/
 NProgress.configure({ showSpinner: false });
 
 router.beforeResolve((to, from, next) => {
-  if (to.name) {
-    NProgress.start();
-  }
+     if (to.name) {
+          NProgress.start();
+     }
 
-  next();
+     next();
 });
 
 router.afterEach(() => {
-  NProgress.done();
+     NProgress.done();
 });
 /*eslint-enable no-unused-vars*/
 
